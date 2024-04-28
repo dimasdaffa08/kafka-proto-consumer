a15d7fccc4538de16e2c1a928f90e8eb805e3537
dimasdaffa08 <Dimas.Muhammad.Daffa@bni.co.id>
2024-04-28 15:32:18 +0700

    Initial Commit

---
+++ src/main/java/com/dev/dmd/kafkaprotoconsumer/KafkaProtoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dmd.kafkaprotoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProtoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProtoConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/dmd/kafkaprotoconsumer/KafkaProtoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dmd.kafkaprotoconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProtoConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
